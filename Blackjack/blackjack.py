9af4cab128f51c067180049905e6c24744b1ff5d
0xTurk <97168241+TurkTheLegend@users.noreply.github.com>
2022-08-07 21:47:15 +0700

    Add files via upload

---
+++ Blackjack/blackjack.py
@@ -0,0 +1,42 @@
+import random
+
+#สร้างเด็ค
+deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] * 4
+#สับไพ่
+random.shuffle(deck)
+#จั่วไพ่ฝ่ายละ 1 ใบ
+player_hand = []
+dealer_hand = []
+for i in range(2):
+    player_hand.append(deck.pop())
+    dealer_hand.append(deck.pop())
+#เปิดไพ่
+print("Player: " + str(player_hand))
+print("Dealer: " + str(dealer_hand))
+#ตาผู้เล่น
+while True:
+    choice = input("Hit or stand? ")
+    if choice == "hit":
+        player_hand.append(deck.pop())
+        print("Player: " + str(player_hand))
+        if sum(player_hand) > 21:
+            print("You lose!")
+            break
+        elif choice == "stand":
+            break
+        print("Dealer: " + str(dealer_hand))
+#ตาบอท
+while sum(dealer_hand) < 17:
+    dealer_hand.append(deck.pop())
+    print("Dealer: " + str(dealer_hand))
+    if sum(dealer_hand) > 21:
+        print("Dealer loses!")
+        break
+#เปรียบเทียบไพ่
+if sum(player_hand) <= 21 and sum(dealer_hand) <= 21:
+    if sum(player_hand) > sum(dealer_hand):
+        print("You win!")
+    elif sum(player_hand) < sum(dealer_hand):
+        print("You lose!")
+    else:
+        print("Tie!")
